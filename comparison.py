2d5b845fd2305ccbd8fcaad6b84a1a75af7a5c58
Sinnefa <Sinnefa@users.noreply.github.com>
2020-12-10 22:35:23 +0100

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,5 +1,5 @@
-from Levenshtein import *
-from sickLevensthein import *
+import textdistance
+from sickLevenshtein import *
 from levenstheinDP import *
 from recursiveLevensthein import *
 import random
@@ -7,11 +7,11 @@ import time
 
 print("Levenshtein package\tTIME Levenshtein package\tRecursive\tTIME Recursive\tMy memroy optimized Levenshtein\tTIME My memroy optimized Levenshtein\tMy sick Levenshtein\tTIME My sick Levenshtein")
 for i in range(100):
-	a = ''.join(random.choices(["a","c","t","g"], k = random.randint(2,13)))
-	b = ''.join(random.choices(["a","c","t","g"], k = random.randint(2,13)))
+	a = ''.join(random.choices(["a","c","t","g"], k = random.randint(2,10)))
+	b = ''.join(random.choices(["a","c","t","g"], k = random.randint(2,10)))
 	# Levenshtein package
 	start_time = time.time()
-	d1 = distance(a,b)
+	d1 = textdistance.levenshtein(a,b)
 	t1 = time.time() - start_time
 	# Levenshtein recursive lambda
 	start_time = time.time()
@@ -23,7 +23,7 @@ for i in range(100):
 	t3 = time.time() - start_time
 	# My sick Levenshtein
 	start_time = time.time()
-	d4 = mySickLevenshtein(a,b)
+	d4 = sickLevenshtein(a,b)
 	t4 = time.time() - start_time
 	print(str(d1)+"\t"+str(t1)+"\t"+str(d2)+"\t"+str(t2)+"\t"+str(d3)+"\t"+str(t3)+"\t"+str(d4)+"\t"+str(t4)+"\t"+a+"\t"+b)
 	#print(d1==d4)
