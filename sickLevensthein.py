d3cc28e3c5925accb5d24966aa6e4647a49bb63e
Sinnefa <Sinnefa@users.noreply.github.com>
2020-12-10 22:35:57 +0100

    Update sickLevensthein.py

--- sickLevensthein.py
+++ sickLevensthein.py
@@ -2,11 +2,5 @@
 # My sick implementation Levenshtein distance
 # Dynamic Programming in one line
 ##############################################
-def assigner(a,b):
-	a[:]=b[:]
-	return a
-
-def mySickLevenshtein(a,b):
-	# Depends on assigner() as
-	# you can't assign within in-line loops
-	return [[assigner(v1,[assigner(v2,[v2[0]]+[min(v2[i-1]+1,v1[i]+1,v1[i-1]+(b[i-1]!=a[v2[0]-1])) for i in range(1,len(b)+1)]) for i in range(1,len(b)+1)][-1]) for v2 in [[i]+[0 for i in range(0,len(b)+1)][1:] for i in range(1,len(a)+1)]] for v1 in [list(range(0,len(b)+1))]][-1][-1][-1]
+def sickLevenshtein(a,b):
+	return (lambda count,m,f: f(count,m,f))(0,[[c if r==0 else r if c==0 else 0 for c in range(len(a)+1)] for r in range(len(b)+1)],lambda count,m,f: [m:=f(count+1,[[m[i][j] if i==0 or j==0 else min(m[i-1][j]+1,m[i][j-1]+1,m[i-1][j-1]+(a[j-1]!=b[i-1])) for j in range(len(a)+1)] for i in range(len(b)+1)],f)[0] if count<len(a)*len(b) else m])[0][len(b)][len(a)]
